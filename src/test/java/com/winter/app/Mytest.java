873c4e9f9d2eb5d7de9b2fc10623a0e77bf647d4
beomkim7 <beomkim7@naver.com>
2024-01-08 14:03:07 +0900

    1234

---
+++ src/test/java/com/winter/app/Mytest.java
@@ -0,0 +1,21 @@
+package com.winter.app;
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public class Mytest {
+
+	public void test() {
+		
+		
+	}
+}
