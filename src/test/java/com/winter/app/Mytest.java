d8848f260983e64de0410638ff309e3ccf4c0ef3
beomkim7 <beomkim7@naver.com>
2024-01-12 14:38:25 +0900

    초기설정 끝

--- src/test/java/com/winter/app/Mytest.java
+++ src/test/java/com/winter/app/Mytest.java
@@ -12,10 +12,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
-public class Mytest {
+						//location 
+public abstract class Mytest {
 
-	public void test() {
-		
-		
-	}
+	
 }
